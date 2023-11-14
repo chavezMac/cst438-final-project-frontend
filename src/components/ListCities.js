550fb86d8dba3cae9dc656dfbc7f5bcbd82a5ec4
RyRom <109193666+RyRom@users.noreply.github.com>
2023-11-14 08:32:26 -0800

    Update ListCities.js
    Updated to have add city button and work with CitySelectionModal.js

--- src/components/ListCities.js
+++ src/components/ListCities.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {SERVER_URL} from '../constants';
 import {Link} from 'react-router-dom';
 import {apikey} from '../constants';
+import CitySelectionModal from './CitySelectionModal';
 
 function ListCities(props) {
 
   const [cities, setCities] = useState([]);
   const [message, setMessage] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
    // called once after intial render
@@ -15,7 +17,8 @@ function ListCities(props) {
  
   const fetchCities = async () => {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${apikey}`);
+      // const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${apikey}`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${apikey}`)
       const data = await response.json();
       console.log(data);
 
@@ -23,23 +26,39 @@ function ListCities(props) {
     } catch (error) {
       setMessage('Error retrieving cities');
     }
-  }
-  
-  
-    const headers = ['City', 'Temperature', '', ' ', ' ', ' '];
-
-    return (
-    
-      <div>
-        <h1>Weather Stats for {cities.length > 0 ? cities[0].name : 'City'}</h1>
-        {cities.length > 0 && (
-          <div>
-            <h2>{cities[0].name}</h2>
-            <p>Temperature: {cities[0].data.current.temp} °F</p>
-          </div>
-        )}
-      </div>
-    );
   };
+
+  const handleAddCity = (newCity) => {
+    // Implement logic to add the city
+    console.log(`Adding city: ${newCity}`);
+    // Make API call or update state as needed
+    setIsModalOpen(false);
+  };
+  
+  const headers = ['City', 'Temperature', '', ' ', ' ', ' '];
+
+  return (
+  
+    <div>
+      <h1>Weather Stats </h1>
+      <button onClick={() => setIsModalOpen(true)}>Add City</button>
+      {cities.length > 0 && (
+        <div>
+          <h2>{cities[0].name}</h2>
+          <p>Temperature: {cities[0].data.main.temp} °F</p>
+        </div>
+      )}
+      <br/>
+      <br/>
+
+      {isModalOpen && (
+      <CitySelectionModal
+        onClose={() => setIsModalOpen(false)}
+        onCitySelect={handleAddCity}
+      />
+      )}
+    </div>
+  );
+};
 
 export default ListCities;
