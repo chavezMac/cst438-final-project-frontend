2380fc9b6734a1fef742c78a2e3aca71cff5bf31
Michael <chavom68@gmail.com>
2023-11-26 14:25:03 -0800

    Frontend for deleting and adding a city implement

--- src/components/UserCities.js
+++ src/components/UserCities.js
@@ -1,44 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { SERVER_URL } from '../constants';
 import '../Dashboard.css';
 import CitySelectionModal from './CitySelectionModal';
 
-function UserCities(props) {
+function CityManager(props) {
   const [cities, setCities] = useState([]);
   const [message, setMessage] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddCityClicked, setIsAddCityClicked] = useState(false);
+  const [isDeleteCityClicked, setIsDeleteCityClicked] = useState(false)
+  const url = SERVER_URL + '/city/' + props.user.user_id;
+
+  const fetchUserCities = useCallback( async () => {
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: sessionStorage.getItem('jwt'),
+        },
+      });
+
+      if (response.ok) {
+        const cities = await response.json();
+        setCities(cities);
+      } else {
+        console.error('Fails on first render, ignore this....', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching cities', error);
+    }
+  },[url]); 
 
   useEffect(() => {
-    const url = SERVER_URL + '/city/' + props.user.user_id;
-
-    const fetchUserCities = async () => {
-      try {
-        const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: sessionStorage.getItem('jwt'),
-          },
-        });
-
-        if (response.ok) {
-          const cities = await response.json();
-          setCities(cities);
-        } else {
-          console.error('Fails on first render, ignore this....', response.statusText);
-        }
-      } catch (error) {
-        console.error('Error fetching cities', error);
-      }
-    };
-
     fetchUserCities();
-  }, [props.user.user_id]); 
+  },[props.user.user_id, fetchUserCities]);
   
   function handleAddCity(newCity) {
     console.log(`Adding city: ${newCity}`);
     const data = new URLSearchParams();
     data.append('name', newCity);
+
     fetch(`${SERVER_URL}/city/${props.user.user_id}`, {
       method: 'POST',
       headers: {
@@ -50,18 +52,53 @@ function UserCities(props) {
       .then((response) => {
         if (response.ok) {
           setMessage(`Added city: ${newCity}`);
-          console.log(response.json());
         }
       }).then((responseData) => {
         console.log(responseData);
-      }).catch((err) => console.error(err));
-    setIsModalOpen(false);
+      }).catch((err) => console.error(err))
+      .finally(() => {
+        setIsModalOpen(false);
+        setIsAddCityClicked(false);
+        fetchUserCities();
+      });
+  }
+
+  function handleDeleteCity(newCity) {
+    console.log(`Deleting city: ${newCity}`);
+    const data = new URLSearchParams();
+    data.append('name', newCity);
+    console.log(props.user.user_id);
+    fetch(`${SERVER_URL}/city/${props.user.user_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: sessionStorage.getItem('jwt'),
+      },
+      body: data,
+    })
+      .then((response) => {
+        if (response.ok) {
+          setMessage(`Deleted city: ${newCity}`);
+        }
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        setIsModalOpen(false);
+        setIsDeleteCityClicked(false);
+        fetchUserCities();
+      });
+  }
+
+  function logout() {
+    sessionStorage.removeItem('jwt');
+    window.location.reload();
   }
 
   return (
       <div className="Dashboard">
           <h3>Cities</h3>
-          <button onClick={() => setIsModalOpen(true)}>Add City</button>
+          <button onClick={() => {setIsModalOpen(true); setIsAddCityClicked(true);}}>Add City</button>
+          <button onClick={() => {setIsModalOpen(true); setIsDeleteCityClicked(true);}}>Delete City</button>
           <div className="card-container">
               {cities.map((city, index) => (
                   <div className="card" key={index}>
@@ -75,13 +112,15 @@ function UserCities(props) {
           </div>
           <div>{message}</div>
           {isModalOpen && (
-        <CitySelectionModal
-          onClose={() => setIsModalOpen(false)}
-          onCitySelect={handleAddCity}
-        />
+            <CitySelectionModal
+            onClose={() => setIsModalOpen(false)}
+            onCitySelect={isAddCityClicked ? handleAddCity : undefined}
+            onDeleteCity={isDeleteCityClicked ? handleDeleteCity : undefined}
+          />
       )}
+      <button onClick={logout}>Logout</button>
       </div>
   );
 }
 
-export default UserCities;
+export default CityManager;
